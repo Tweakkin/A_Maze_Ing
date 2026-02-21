95221f627411067c856a1b78fd551df05f16cd2a
Tweakkin <yahyaboukhmira7@gmail.com>
2026-02-21 18:04:20 +0000

    Adjusted the a_maze_ing.py to work with the class that parses everything instead of calling each method individually

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,30 +1,34 @@
 import sys
-from config_parser import parse_coordinate, file_to_dict, val_keys, val_dimensions, val_bool, val_file
+from config_parser import ConfigPasrer
+
+class MazeGenerator:
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self.height = height
+
+        self.grid = []
+        for _ in range(height):
+            temp = []
+            for _ in range(width):
+                temp.append(15)
+            self.grid.append(temp)
+    
+    def get_cell(self, x:int, y:int) -> int:
+        return self.grid[x][y]
+
+    
 
 if __name__ == "__main__":
 
-	# sys.argv must contain exactly 2 items: [script_name, config_file]
-	if len(sys.argv) != 2:
-		print("Error: Usage is python3 a_maze_ing.py <config_file>")
-		sys.exit(1)
-	
-	#Turning the file into a dict
-	parsed_dict = file_to_dict(sys.argv[1])
-
-	#Checking for missing or unsupported keys
-	bon_keys = val_keys(parsed_dict)
-	
-	#Validting the values of HEIGHT AND WIDTH
-	parsed_dict = val_dimensions(parsed_dict)
-	
-	#Checking if ENTRY and EXIT values are valid
-	parsed_dict['ENTRY'] = parse_coordinate(parsed_dict['ENTRY'], parsed_dict['HEIGHT'], parsed_dict['WIDTH'])
-	parsed_dict['EXIT'] = parse_coordinate(parsed_dict['EXIT'], parsed_dict['HEIGHT'], parsed_dict['WIDTH'])
-	if parsed_dict['ENTRY'] == parsed_dict['EXIT']:
-		print("Error: 'ENTRY' and 'EXIT' coordinates cannot be the same!")
-		sys.exit(1)
-	#Validating 'PERFECT' value	
-	parsed_dict = val_bool(parsed_dict)
-	
-	#Validating 'OUTPUT_FILE'
-	val_file(parsed_dict)
+    # sys.argv must contain exactly 2 items: [script_name, config_file]
+    if len(sys.argv) != 2:
+        print("Error: Usage is python3 a_maze_ing.py <config_file>")
+        sys.exit(1)
+    
+    toparse = ConfigPasrer(sys.argv[1])
+    toparse.parse()
+    
+    gen = MazeGenerator(toparse.parsed_dict['WIDTH'], toparse.parsed_dict['HEIGHT'])
+    
+    a = 15
+    print(5 & 2)
